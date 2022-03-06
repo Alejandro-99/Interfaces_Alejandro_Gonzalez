74436667f489939f6a08a68bd933d856419fd1fc
Alejandro-99 <alexgh859911@gmail.com>
2022-03-06 19:25:23 +0100

    Ejercicio 2 C update

--- Ejercioo 2 Controles/src/application/MainLayouts_Controles.java
+++ Ejercioo 2 Controles/src/application/MainLayouts_Controles.java
@@ -13,12 +13,12 @@ public class MainLayouts_Controles extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			// Carga el diseño del archivo FXML en la variable rootLayout
+			
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainLayouts_Controles.class.getResource("MainLayouts_Controles.fxml"));
 			rootLayout = (BorderPane) loader.load();
 
-			// Mostramos la escena del BorderPane de la variable rootLayot
+			
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Ejercicio sobre layouts y controles");
