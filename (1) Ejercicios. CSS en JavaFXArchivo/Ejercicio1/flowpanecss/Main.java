696664bebc4979dd97975d81572967f55a2e4c4a
Alejandro-99 <alexgh859911@gmail.com>
2022-04-04 16:49:58 +0200

    Ejercicio 1

---
+++ (1) Ejercicios. CSS en JavaFXArchivo/Ejercicio1/flowpanecss/Main.java
@@ -0,0 +1,37 @@
+package flowpanecss;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	private FlowPane rootLayout;	
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Carga el diseño del archivo FXML en la variable rootLayout
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("FlowPaneLayout.fxml"));
+			rootLayout = (FlowPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			scene.getStylesheets().add("flowpanecss/style.css");
+			primaryStage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
+}
